bf54e7b7e0be682117140079985252918945e5a1
Jair Junior <jairjunior@grupoperola.local>
2018-11-20 17:56:50 -0200

    terceiro commit

--- src/Model/Cliente.java
+++ src/Model/Cliente.java
@@ -5,10 +5,53 @@
  */
 package Model;
 
+import java.sql.Date;
+
 /**
  *
  * @author jairjunior
  */
 public class Cliente {
+    
+    private int codcadastroclientes;
+    private String nome;
+    private Date nascimento;
+    private boolean ativo;
+    private String endereco;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String complemento;
+    private String cep;
+    private String email;
+    private String site;
+    private String pontoreferencia;
+    private boolean ativaconveniado;
+    private String telefone;
+    private String celular;
+    private String fax;
+    private boolean fisica;
+    private boolean juridica;
+    private String cpf;
+    private String rg;
+    private String cnpj;
+    private String incricaoestadual;
+    private String observacoes;
+    private Date datacadastro;
+    private float limitecredito;
+    private float totalfaturas;
+    private float juros;
+    private float multa;
+    private Date diabompagamento;
+    private String enderecoentrega;
+    private int numeroentrega;
+    private String bairroentrega;
+    private String cidadeentrega;
+    private String estadoentrega;
+    private String complementoentrega;
+    private String cepentrega;
+    private String pontoreferenciaentrega;
+    
     
 }
